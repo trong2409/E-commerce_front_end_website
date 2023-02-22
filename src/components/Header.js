8ebb0dae280e71279fccddc73ec6c0d5eb2e51fd
trong2409 <nvtrong249@gmail.com>
2023-02-22 23:48:37 +0700

    code for header, footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,53 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
-
-function header() {
+import compare from "../images/compare.svg";
+import wishlist from "../images/wishlist.svg";
+import user from "../images/user.svg";
+import cart from "../images/cart.svg";
+import menu from "../images/menu.svg";
+const Header = () => {
   return (
     <>
       <header className="header-top-strip py-3">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6">
+            <div className="d-none d-md-block col-md-6">
               <p className="text-white mb-0">
-                Free shipping over $100 & Free Returns
+                Free Shipping Over $100 & Free Returns
               </p>
             </div>
-            <div className="col-6 text-end ">
-              <p className="text-white mb-0">
-                Hotline:{" "}
-                <a className="text-white" href="tel:+84 123456789">
-                  +84 123456789
+            <div className="col-4 d-md-none">
+              <h2 className="mb-0">
+                <Link className="text-white">Digitic</Link>
+              </h2>
+            </div>
+            <div className="col-8 col-md-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+91 8264954234">
+                  +91 8264954234
                 </a>
               </p>
             </div>
           </div>
         </div>
       </header>
-      <header className="header-upper py-3">
-        <div className="container-xxl ">
+      <header className="header-upper">
+        <div className="container-xxl">
           <div className="row align-items-center">
-            <div className="col-2">
+            <div className="d-none d-md-block col-md-2 col-xl-2 py-3">
               <h2 className="mb-0">
                 <Link className="text-white">Digitic</Link>
               </h2>
             </div>
-            <div className="col-5">
+            <div className="col-md-10 col-xl-5 py-3">
               <div className="input-group">
                 <input
                   type="text"
                   className="form-control py-2"
-                  placeholder="Search product here..."
-                  aria-label="Search product here..."
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
                   aria-describedby="basic-addon2"
                 />
                 <span className="input-group-text p-3" id="basic-addon2">
@@ -46,12 +55,115 @@ function header() {
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-md-12 col-xl-5">
+              <div className="header-upper-links row">
+                <div className="col-6 col-md-3 d-flex justify-content-center py-3">
+                  <Link
+                    to="/compare-product"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
+                    <img src={compare} alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div className="col-6 col-md-3 d-flex justify-content-center py-3">
+                  <Link
+                    to="/wishlist"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
+                    <img src={wishlist} alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div className="col-6 col-md-3 d-flex justify-content-center py-3">
+                  <Link
+                    to="/login"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
+                    <img src={user} alt="user" />
+                    <p className="mb-0">
+                      Log in <br />
+                      Account
+                    </p>
+                  </Link>
+                </div>
+                <div className="col-6 col-md-3 d-flex justify-content-center py-3">
+                  <Link
+                    to="/cart"
+                    className="d-flex align-items-center gap-10 text-white"
+                  >
+                    <img src={cart} alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div className="d-none d-md-block">
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src={menu} alt="" />
+                      <span className="me-5 d-inline-block">
+                        Shop Categories
+                      </span>
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/product">Our Store</NavLink>
+                    <NavLink to="/blogs">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
     </>
   );
-}
+};
 
-export default header;
+export default Header;
